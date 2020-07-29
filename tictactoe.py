e6c6639dd01fe2a79c06275033e7edc6bda8db88
Erdaulet <47812867+Can1s@users.noreply.github.com>
2020-07-29 23:11:35 +0600

    Stage 5/5
    Complete all stages and project

--- tictactoe.py
+++ tictactoe.py
@@ -1,16 +1,11 @@
 def Print_Table(line):
-    array = list(line)
     print("---------")
     for k in range(0, 7, 3):
-        print(f"| {array[k]} {array[k + 1]} {array[k + 2]} |")
+        print(f"| {line[k]} {line[k + 1]} {line[k + 2]} |")
     print("---------")
 
 
-def Check_Status(line):
-    empty_cells = line.count('_')
-    count_x = line.count('X')
-    count_o = line.count('O')
-    array = list(line)
+def Check_Status(array):
     wins_x = False
     wins_o = False
     if ((array[0] == 'O' and array[1] == 'O' and array[2] == 'O')
@@ -32,18 +27,85 @@ def Check_Status(line):
             or (array[6] == 'X' and array[4] == 'X' and array[2] == 'X')):
         wins_x = True
 
-    if wins_x and wins_o or abs(count_o - count_x) > 1:
-        print("Impossible")
-    elif wins_x:
-        print("X wins")
+    if wins_x:
+        return "XW"
     elif wins_o:
+        return "OW"
+    if ' ' not in array:
+        return "DW"
+
+
+array_main = [x for x in '         ']
+Print_Table(array_main)
+while True:
+    if Check_Status(array_main) == "XW":
+        print("X wins")
+        break
+    elif Check_Status(array_main) == "OW":
         print("O wins")
-    elif empty_cells > 0:
-        print("Game not finished")
-    else:
+        break
+    elif Check_Status(array_main) == "DW":
         print("Draw")
-
-
-cells = input("Enter cells: ")
-Print_Table(cells)
-Check_Status(cells)
+        break
+    coordinates = input("Enter the coordinates: ")
+    if coordinates.isalpha():
+        print("You should enter numbers!")
+    else:
+        array_coordinates = coordinates.split()
+        if int(array_coordinates[0]) > 3 or int(array_coordinates[1]) > 3:
+            print("Coordinates should be from 1 to 3!")
+        else:
+            if int(array_coordinates[0]) == 1 and int(array_coordinates[1]) == 3:
+                if array_main[0] != ' ':
+                    print("This cell is occupied! Choose another one!")
+                else:
+                    array_main[0] = 'X'
+                    Print_Table(array_main)
+            elif int(array_coordinates[0]) == 2 and int(array_coordinates[1]) == 3:
+                if array_main[1] != ' ':
+                    print("This cell is occupied! Choose another one!")
+                else:
+                    array_main[1] = 'X'
+                    Print_Table(array_main)
+            elif int(array_coordinates[0]) == 3 and int(array_coordinates[1]) == 3:
+                if array_main[2] != ' ':
+                    print("This cell is occupied! Choose another one!")
+                else:
+                    array_main[2] = 'X'
+                    Print_Table(array_main)
+            elif int(array_coordinates[0]) == 1 and int(array_coordinates[1]) == 2:
+                if array_main[3] != ' ':
+                    print("This cell is occupied! Choose another one!")
+                else:
+                    array_main[3] = 'X'
+                    Print_Table(array_main)
+            elif int(array_coordinates[0]) == 2 and int(array_coordinates[1]) == 2:
+                if array_main[4] != ' ':
+                    print("This cell is occupied! Choose another one!")
+                else:
+                    array_main[4] = 'X'
+                    Print_Table(array_main)
+            elif int(array_coordinates[0]) == 3 and int(array_coordinates[1]) == 2:
+                if array_main[5] != ' ':
+                    print("This cell is occupied! Choose another one!")
+                else:
+                    array_main[5] = 'X'
+                    Print_Table(array_main)
+            elif int(array_coordinates[0]) == 1 and int(array_coordinates[1]) == 1:
+                if array_main[6] != ' ':
+                    print("This cell is occupied! Choose another one!")
+                else:
+                    array_main[6] = 'X'
+                    Print_Table(array_main)
+            elif int(array_coordinates[0]) == 2 and int(array_coordinates[1]) == 1:
+                if array_main[7] != ' ':
+                    print("This cell is occupied! Choose another one!")
+                else:
+                    array_main[7] = 'X'
+                    Print_Table(array_main)
+            elif int(array_coordinates[0]) == 3 and int(array_coordinates[1]) == 1:
+                if array_main[8] != ' ':
+                    print("This cell is occupied! Choose another one!")
+                else:
+                    array_main[8] = 'X'
+                    Print_Table(array_main)
